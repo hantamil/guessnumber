7ea44c128fc73d6623478c5e2436a7a5c86c11d3
hantamil <mira0004@stud.kea.dk>
2024-09-20 23:32:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("i am in guessnumber")
+
+const lblMessage = document.querySelector(".message");
+console.log(lblMessage);
+console.log(lblMessage.textContent)
+
+const lblNumber = document.querySelector(".number");
+console.log(lblNumber);
+
+const lblScore = document.querySelector(".score");
+console.log(lblScore);
